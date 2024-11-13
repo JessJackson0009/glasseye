a0c6f2688e47a48163854da42256c7a0ae48ee1c
JessJackson0009 <jess.jackson0009@gmail.com>
2024-11-12 20:47:26 -0500

    fixed centering issue on about page

--- app/Contact/page.tsx
+++ app/Contact/page.tsx
@@ -14,25 +14,35 @@ export default function Contact() {
         />
         <h1 className={styles.contactHeading}>How to Get in Touch</h1>
       </div>
-      <div className={styles.contactsContainer}>
-        <div className={styles.cardContainer}>
-          <img src="/phone.png" alt="Phone Logo" className={styles.cardLogo} />
-          <h3 className={styles.cardTitle}>By Phone</h3>
-          <p className={styles.cardText}>(904)693-9868</p>
-        </div>
-        <div className={styles.cardContainer}>
-          <img src="/email.png" alt="Email Logo" className={styles.cardLogo} />
-          <h3 className={styles.cardTitle}>By Email</h3>
-          <p className={styles.cardText}>Theglasseye@comcast.net</p>
-        </div>
-        <div className={styles.cardContainer}>
-          <img
-            src="/gram.png"
-            alt="Instagram Logo"
-            className={styles.cardLogo}
-          />
-          <h3 className={styles.cardTitle}>By Instagram</h3>
-          <p className={styles.cardText}>@glasseye76</p>
+      <div className={styles.mainContainer}>
+        <div className={styles.contactsContainer}>
+          <div className={styles.cardContainer}>
+            <img
+              src="/phone.png"
+              alt="Phone Logo"
+              className={styles.cardLogo}
+            />
+            <h3 className={styles.cardTitle}>By Phone</h3>
+            <p className={styles.cardText}>(904)693-9868</p>
+          </div>
+          <div className={styles.cardContainer}>
+            <img
+              src="/email.png"
+              alt="Email Logo"
+              className={styles.cardLogo}
+            />
+            <h3 className={styles.cardTitle}>By Email</h3>
+            <p className={styles.cardText}>Theglasseye@comcast.net</p>
+          </div>
+          <div className={styles.cardContainer}>
+            <img
+              src="/gram.png"
+              alt="Instagram Logo"
+              className={styles.cardLogo}
+            />
+            <h3 className={styles.cardTitle}>By Instagram</h3>
+            <p className={styles.cardText}>@glasseye76</p>
+          </div>
         </div>
       </div>
       <p className={styles.bottomText}>
